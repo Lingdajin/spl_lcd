c8a697390243ec7f7f1cf09de920f74fcef2ac1f
lingdajin <Ling_da_jin@bupt.edu.cn>
2025-01-16 19:59:12 +0800

    add seg_led to test

--- impl1/spi_lcd_impl1_map.asd
+++ impl1/spi_lcd_impl1_map.asd
@@ -3,13 +3,13 @@ Device = LCMXO2-4000HC;
 Package = CSBGA132;
 Performance = 5;
 LUTS_avail = 4320;
-LUTS_used = 1466;
+LUTS_used = 1475;
 FF_avail = 4425;
-FF_used = 225;
+FF_used = 230;
 INPUT_LVCMOS25 = 2;
-OUTPUT_LVCMOS25 = 8;
+OUTPUT_LVCMOS25 = 17;
 IO_avail = 105;
-IO_used = 10;
+IO_used = 19;
 EBR_avail = 10;
 EBR_used = 0;
 ; Begin PLL Section
--- source/spi_lcd.v
+++ source/spi_lcd.v
@@ -27,7 +27,8 @@ module  spi_lcd
     output          lcd_cs        ,
     output          lcd_led       ,
     output          debug_led1    ,
-    output          debug_led2    
+    output          debug_led2    ,
+    output 	  [8:0] segment_led
 );
 wire            sys_clk_50MHz;
 wire    [8:0]   data;   
@@ -54,11 +55,25 @@ assign  lcd_led = 1'b1;
 assign debug_led1 = init_done;
 assign debug_led2 = show_char_done;
 
-always@(posedge sys_clk_50MHz or negedge sys_rst_n)
+reg [3:0] count;
+wire [3:0] seg_data;
+
+assign seg_data = count;
+
+always@(posedge show_char_done or negedge sys_rst_n)
     if(!sys_rst_n)
-        debug_led2 <= 0;
+        count <= 'd0;
+    else if(count > 'd0)
+        count <= 'd0;
     else
+        count <= count + 1'b1;
         
+
+segment_led segment_led_inst
+(
+    .seg_data(seg_data),		//seg_data input
+    .segment_led(segment_led)	
+);
 
 pll pll_u1
 (
